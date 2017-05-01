b0fc84b03f9887101ce3b089974cc09bae5e77ef
Joyce Mayorga <joycem137@gmail.com>
2017-05-01 10:03:19 -0700

    Added array shuffler

--- index.js
+++ index.js
@@ -17,5 +17,22 @@ module.exports = {
      */
     checkClamped: function(value, min, max) {
         return value >= min && value <= max;
+    },
+
+    /**
+     * Randomly shuffles the indicated array.
+     * From: https://www.frankmitchell.org/2015/01/fisher-yates/
+     */
+    shuffleArray: function(array) {
+        var i = 0
+            , j = 0
+            , temp = null;
+
+        for (i = array.length - 1; i > 0; i -= 1) {
+            j = Math.floor(Math.random() * (i + 1));
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp
+        }
     }
 };
